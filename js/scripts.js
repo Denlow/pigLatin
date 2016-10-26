0716f5118d81639ee9304e24c8264420022b759a
Dave Denlow and Levi Bibo <dave@>
2016-10-26 11:49:17 -0700

    add ability to recognize words with consecutive starting consonants.

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,33 @@ function isLetter(character) {
   }
 }
 
+function firstVowel(word) {
+  var wordArray = word.split("");
+  var indexCounter = 0;
+  var indexFound = false;
+  wordArray.forEach(function(letter){
+    if (isVowel(letter) && !indexFound){
+      indexFound = true;
+    } else if (isLetter(letter) && !indexFound) {
+      if (letter.toLowerCase() === "q" && word[indexCounter+1] === "u") {
+        if (isVowel(word[indexCounter+2])) {
+          indexCounter += 2;
+          indexFound = true;
+        } else {
+          indexCounter += 2;
+        };
+      } else if (!indexFound) {
+        indexCounter++;
+      }
+    }
+    else if (!indexFound) {
+      indexCounter++;
+    }
+  });
+
+  return indexCounter;
+}
+
 function isVowel(character) {
   var valid = false;
   vowelArray.forEach(function(vowel) {
@@ -35,7 +62,6 @@ function isVowel(character) {
 }
 
 function appendSuffix(string) {
-  var firstLetter;
   if (string.length === 1) {
     if (isLetter(string)) {
       return string + "ay";
@@ -43,17 +69,19 @@ function appendSuffix(string) {
   } else if (isVowel(string[0])) {
     return string + "ay";
   } else if (isLetter(string[0])) {
-    if (string.slice(0, 2).toLowerCase() === "qu") {
-      firstLetter = string.slice(0, 2);
-      var newWord = string.slice(2, string.length);
-      newWord += firstLetter;
-      return newWord + "ay";
-    } else {
-      firstLetter = string[0];
-      var newWord = string.slice(1, string.length);
-      newWord += firstLetter;
-      return newWord + "ay";
-    };
+    // if (string.slice(0, 2).toLowerCase() === "qu") {
+    //   firstLetter = string.slice(0, 2);
+    //   var newWord = string.slice(2, string.length);
+    //   newWord += firstLetter;
+    //   return newWord + "ay";
+    // } else {
+    console.log(firstVowel(string));
+    var moveLetters = string.slice(0, firstVowel(string));
+    console.log(firstVowel(string));
+    var newWord = string.slice(firstVowel(string), string.length);
+    newWord += moveLetters;
+    return newWord + "ay";
+    //};
   };
 };
 
